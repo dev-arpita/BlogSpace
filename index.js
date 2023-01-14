c3dff8b174e0f2c47317c410405b0a146c65a0f4
Arpita Roy <arpita.roy.workar@gmail.com>
2023-01-14 20:57:21 +0530

    BlogSpace - New post form

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-/**
- Challenge:
-
- Style it up!
-
- * Add a short (~30px height) fixed navbar at the top with the text "BlogSpace". Remember to pad the top of your content so it doesn't get hidden behind the navbar.
- * Add a font from Google Fonts.
- * Any other styling you want to make it look nice!
-
- */
-
 fetch("https://apis.scrimba.com/jsonplaceholder/posts")
   .then((res) => res.json())
   .then((data) => {
