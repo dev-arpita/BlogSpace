e610e4edad74610c2b41d8d8138f1b9b1c8802da
Arpita Roy <arpita.roy.workar@gmail.com>
2023-01-20 20:31:37 +0530

    BlogSpace - Form submit eventListner

--- index.js
+++ index.js
@@ -12,3 +12,25 @@ fetch("https://apis.scrimba.com/jsonplaceholder/posts")
     }
     document.getElementById("blog-list").innerHTML = html;
   });
+
+
+/**
+ Challenge:
+
+ * Listen for the "submit" event on the form (which will happen when the button is clicked)
+    * (Don't forget to preventDefault on the form so it doesn't refresh your page. Google "form preventDefault" if you're not sure what I'm talking about)
+ * Combine the title value and body value into an object (with a "title" property and "body" property)
+ * Log the object to the console
+
+*/
+
+document.getElementById("new-post").addEventListener("submit", function(e) {
+    e.preventDefault()
+    const postTitle = document.getElementById("post-title").value
+    const postBody = document.getElementById("post-body").value
+    const data = {
+        title: postTitle,
+        body: postBody
+    }
+    console.log(data)
+})
