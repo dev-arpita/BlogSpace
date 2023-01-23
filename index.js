d0655bc6cb0004cd2ed831a15bb116b59bbb3dec
Arpita Roy <arpita.roy.workar@gmail.com>
2023-01-23 11:03:31 +0530

    Requests - Headers

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 /**
-Challenge:
-
-- Send a request to add a new todo item
-    - BaseURL: https://apis.scrimba.com/jsonplaceholder/
-    - Endpoint: /todos
-- Body object should contain a "title" property (string)
-  and a "completed" property (boolean)
-- Don't add the .then() blocks yet - we have 1 more thing
-  to do before it'll work correctly
-
-Part 2 - add the .then blocks to check the data coming back
-
-*/
+ * Challenge:
+ *
+ * Check this website for how to change the Content-Type header
+ * to indicate that there's JSON in the request body.
+ *
+ * https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch#supplying_request_options
+ *
+ * Apply that header to our fetch request, send it, and check what comes back.
+ */
 
 fetch("https://apis.scrimba.com/jsonplaceholder/todos", {
     method: "POST",
     body: JSON.stringify({
         title: "Buy Milk",
         completed: false
-    })
+    }),
+    headers: {
+        "Content-Type" : "application/json"
+    }
 })
     .then(res => res.json())
     .then(data => console.log(data))
