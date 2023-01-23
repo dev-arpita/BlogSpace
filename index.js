34321e5a410df4b62fcc4533ed1bbedc2f27a467
Arpita Roy <arpita.roy.workar@gmail.com>
2023-01-23 11:30:55 +0530

    BlogSpace - send new post to server

--- index.js
+++ index.js
@@ -1,23 +1,43 @@
-/**
- * Challenge:
- *
- * Check this website for how to change the Content-Type header
- * to indicate that there's JSON in the request body.
- *
- * https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch#supplying_request_options
- *
- * Apply that header to our fetch request, send it, and check what comes back.
- */
-
-fetch("https://apis.scrimba.com/jsonplaceholder/todos", {
-    method: "POST",
-    body: JSON.stringify({
-        title: "Buy Milk",
-        completed: false
-    }),
-    headers: {
-        "Content-Type" : "application/json"
-    }
-})
+fetch("https://apis.scrimba.com/jsonplaceholder/posts")
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => {
+        const postsArr = data.slice(0, 5)
+        let html = ""
+        for (let post of postsArr) {
+            html += `
+                <h3>${post.title}</h3>
+                <p>${post.body}</p>
+                <hr />
+            `
+        }
+        document.getElementById("blog-list").innerHTML = html
+    })
+
+document.getElementById("new-post").addEventListener("submit", function(e) {
+    e.preventDefault()
+    const postTitle = document.getElementById("post-title").value
+    const postBody = document.getElementById("post-body").value
+    const data = {
+        title: postTitle,
+        body: postBody
+    }
+
+    fetch("https://apis.scrimba.com/jsonplaceholder/posts", {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    .then(res => res.json())
+    .then( data => console.log(data))
+    /**
+     * Challenge: Send this off to the server!
+     *
+     * 1. BaseURL: https://apis.scrimba.com/jsonplaceholder/
+     * 2. Endpoint: /posts
+     * 3. method: ???
+     * 4. Request body: ??? (Remember to turn it into JSON)
+     * 5. Headers: ??? (Check the JSON Placeholder API docs or past casts for help)
+     */
+})
