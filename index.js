5b9d98a7c2c11757533358cfdbaba32929af2b17
Arpita Roy <arpita.roy.workar@gmail.com>
2023-01-30 18:03:02 +0530

    URL parameters - JSON placeholder API

--- index.js
+++ index.js
@@ -1,55 +1,9 @@
-let postsArray = []
-const titleInput = document.getElementById("post-title")
-const bodyInput = document.getElementById("post-body")
-const form = document.getElementById("new-post")
-
-function renderPosts() {
-    let html = ""
-    for (let post of postsArray) {
-        html += `
-            <h3>${post.title}</h3>
-            <p>${post.body}</p>
-            <hr />
-        `
-    }
-    document.getElementById("blog-list").innerHTML = html
-}
-
-fetch("https://apis.scrimba.com/jsonplaceholder/posts")
+/**
+ * Challenge: GET all the comments from the blog post with ID of 2 and log to the console
+ *
+ * BaseURL: https://apis.scrimba.com/jsonplaceholder/
+ * Endpoint: ??? (Check JSON Placeholder docs: https://jsonplaceholder.typicode.com/guide/ and look for the "Listing nested resources" section)
+ */
+fetch("https://apis.scrimba.com/jsonplaceholder/post/2/comments")
     .then(res => res.json())
-    .then(data => {
-        postsArray = data.slice(0, 5)
-        renderPosts()
-    })
-
-form.addEventListener("submit", function(e) {
-    e.preventDefault()
-    const postTitle = titleInput.value
-    const postBody = bodyInput.value
-    const data = {
-        title: postTitle,
-        body: postBody
-    }
-
-    const options = {
-        method: "POST",
-        body: JSON.stringify(data),
-        headers: {
-            "Content-Type": "application/json"
-        }
-    }
-
-    fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
-        .then(res => res.json())
-        .then(post => {
-            postsArray.unshift(post)
-            renderPosts()
-            /**
-             * Challenge: clear the form out!
-             */
-            // titleInput.value = ""
-            // bodyInput.value = ""
-            form.reset()
-
-        })
-})
+    .then(data => console.log(data))
